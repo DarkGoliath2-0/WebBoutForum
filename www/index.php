a2097d8445d94e1379d6c44b1f41f0c5d7a61f89
DarkGoliath2-0 <168300186+DarkGoliath2-0@users.noreply.github.com>
2024-05-25 22:57:52 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -35,7 +35,34 @@
             text-decoration: none;
             color: #7289DA;
         }
+
+        /* Styles pour le pop-up */
+        #popup {
+            display: none;
+            position: fixed;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: 80%;
+            height: 80%;
+            background: white;
+            border: 1px solid #ccc;
+            padding: 20px;
+            overflow: auto;
+            z-index: 1000;
+        }
+        #popup-overlay {
+            display: none;
+            position: fixed;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5);
+            z-index: 999;
+        }
     </style>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
     
@@ -46,12 +73,36 @@
             <br>
             - Voir les instructions d'installation de la boutique en ligne et du forum :
             <br>
-            <a href="README.md" class="instruction">Instruction d'installation de la boutique et du forum</a>
+            <a href="#" class="instruction">Instruction d'installation de la boutique et du forum</a>
             <br>
             Pour plus de renseignements, vous pouvez rejoindre notre serveur Discord :
             <br>
             <a href="https://discord.gg/D4cqeyBHWg" class="discord-link">https://discord.gg/D4cqeyBHWg</a>
         </div>
     </div>
+
+    <div id="popup-overlay"></div>
+    <div id="popup">
+        <button id="close-popup">Fermer</button>
+        <pre id="readme-content"></pre>
+    </div>
+
+    <script>
+        // JavaScript/jQuery pour gérer le pop-up
+        $(document).ready(function() {
+            $('.instruction').click(function(event) {
+                event.preventDefault();
+
+                $.get('README.md', function(data) {
+                    $('#readme-content').text(data);
+                    $('#popup-overlay, #popup').show();
+                });
+            });
+
+            $('#close-popup, #popup-overlay').click(function() {
+                $('#popup-overlay, #popup').hide();
+            });
+        });
+    </script>
 </body>
 </html>
